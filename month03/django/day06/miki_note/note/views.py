c2120559417cd0378909e7134c6afaad6644fed2
hailin <danaotianke@163.com>
2020-07-17 20:29:04 +0800

    django day07

--- month03/django/day06/miki_note/note/views.py
+++ month03/django/day06/miki_note/note/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
+
 # Create your views here.
+def check_loggin(fn):
+    def wrap(request, *args, **kwargs):
+        # 检查登录
+        if 'username' not in request.session or 'uid' not in request.session:
+            # 检查cookies
+            c_username = request.COOKIES.get('username')
+            c_uid = request.COOKIES.get('uid')
+            if not c_username or not c_uid:
+                # 肯定没有登录
+                return HttpResponseRedirect('/user/login')
+            else:
+                # 回写session
+                request.session['username'] = c_username
+                request.session['uid'] = c_uid
+        return fn(request, *args, **kwargs)
+
+    return wrap
+
+
+@check_loggin
+def add_view(request):
+    # 添加笔记
+    return HttpResponse('添加笔记')
---
+++ month03/django/day07/mysite7/mysite7/views.py
@@ -0,0 +1,23 @@
+import time
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+
+
+@cache_page(60)
+def test_cache(request):
+    time.sleep(3)
+    t1= time.time()
+    return  HttpResponse('t1 is %s' % t1)
+
+def test_mw(request):
+    print('----view in -----')
+    return HttpResponse('----test mw is ok')
+
+def test_csrf(request):
+    if request.method == 'GET':
+        return render(request,'test_csrf.html')
+    elif request.method == 'POST':
+        return HttpResponse('---post is ok')
+
+    
